2cf8fe69dc4584b55b276ab43aba0f542d436d94
Sydney Kolehmainen <fsyko001@edu.xamk.fi>
2025-01-25 01:42:57 +0200

    Aiming & Weapon Start

---
+++ Assets/Scripts/Core/Managers/WeaponManager.cs
@@ -0,0 +1,25 @@
+using SLC.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SLC.Core
+{
+    public class WeaponManager : MonoBehaviour
+    {
+
+        public WeaponController[] m_weaponSlots = new WeaponController[2];
+        private InputHandler m_inputHandler;
+
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
